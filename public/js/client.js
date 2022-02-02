3b0da2f339abaa9411df2b311ced7ffd605a89e6
Khang Nguyen <nguyentruongkhang22@gmail.com>
2022-02-03 00:03:34 +0700

    UI updata

--- public/js/client.js
+++ public/js/client.js
@@ -1,16 +1,35 @@
-const msg = document.getElementById("form");
-const socket = io("http://localhost:3001");
+const msg = document.getElementById("text");
+const haha = document.getElementById("metQua");
 
+const submitData = () => {
+    console.log(haha.value);
+};
+
+const socket = io("http://localhost:3001");
+let userName = "";
 socket.on("connect", () => {
     console.log("Successfully connected!");
 });
 
+socket.on("userName", (data) => {
+    userName = data;
+});
+
+// LISTEN TO ENTER
+msg.addEventListener("keydown", (ev) => {
+    if (ev.key === "Enter" && msg.value.length !== 0) {
+        sendMessage();
+    }
+});
+
+// RECEIVE MESSAGE FROM OTHER USERS
 socket.on("message", (e, messager) => {
     appendNewMessage(e, messager);
 });
 
+// SEND NEW MESSAGE FROM THIS USER
 const sendMessage = () => {
-    const message = document.getElementById("text").value;
+    const message = msg.value;
     appendNewMessage(message);
     socket.emit("newMessage", message);
 };
@@ -23,8 +42,14 @@ const appendNewMessage = (message, messager) => {
             ? `
     <div class="d-flex flex-row p-3" style="display: flex; justify-content: end">
         <div class="bg-white mr-2 p-3">
-            <span class="text-muted">${message}.</span>
+        <p class="text-sm-right">${userName}</p>
+            <span class="text-muted"">
+            <p class="alas">
+            ${message}
+            </p>
+            </span>
         </div>
+        
         <img
             src="https://img.icons8.com/color/48/000000/circled-user-male-skin-type-7.png"
             width="30"
@@ -39,7 +64,12 @@ const appendNewMessage = (message, messager) => {
             height="30"
         />
         <div class="bg-white mr-2 p-3">
-            <span class="text-muted">${message}.</span>
+        <p class="text-sm-left">${userName}</p>
+            <span class="text-muted"">
+            <p class="alas">
+            ${message}
+            </p>
+            </span>
         </div>
 
     </div>`;
@@ -49,5 +79,5 @@ const appendNewMessage = (message, messager) => {
     div.scrollIntoView();
 
     // Clear message box after sending message
-    document.getElementById("text").value = "";
+    msg.value = "";
 };
--- server.js
+++ server.js
@@ -1,20 +1,51 @@
+// DEPENDENCIES
 const express = require("express");
 require("pretty-error").start();
-const app = express();
+const morgan = require("morgan");
 const path = require("path");
+
+const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+// SETUPS
 const PORT = 3001 || process.env.PORT;
+const ids = [];
 
+// USING MIDDLEWARES
+const router = app.use(express.Router());
+app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-io.on("connection", (socket) => {
-    console.log(`A new challenger with id: ${socket.id} has approached!`);
-    socket.on("newMessage", (e) => {
-        socket.broadcast.emit("message", e, "others");
-    });
+// ROUTING
+router.route("/chat-room").get((req, res) => {
+    res.sendFile(`${__dirname}./public/chat-room.html`);
 });
 
+router.route("/chat-room").post((req, res) => {
+    console.log(req.body.text);
+    if (req.body.text.length !== 0) {
+        io.on("connection", (socket) => {
+            socket.emit("userName", req.body.text);
+            socket.broadcast.emit("message", "others");
+            ids.push({
+                id: socket.id,
+                name: req.body.text,
+            });
+            console.log(
+                `A new challenger with id: ${socket.id} has approached!\n
+            <============================================================>`
+            );
+            socket.on("newMessage", (e) => {
+                socket.broadcast.emit("message", e, "others");
+            });
+        });
+    }
+    res.sendFile(`${__dirname}/public/chat-room.html`);
+});
+
+// SERVER
 server.listen(PORT, () => {
-    console.log("Good night!");
+    console.log(`Server is running on port: ${PORT}!`);
 });
